d2d01d27ed13e15ba33bf4eff6bfb4575c25e5af
Town Ship <townshipsuncovered@gmail.com>
2025-09-05 18:36:23 +0000

    ZU Dunkel, mache es mit fröhglichen Farben

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="dark">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
