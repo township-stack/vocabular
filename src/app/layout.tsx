2a5db7135a7687d64f77c42cf3cb83ff191570ed
Town Ship <townshipsuncovered@gmail.com>
2025-09-05 18:37:27 +0000

    ich mag den schwarzen Hintergund irgendwie nicht, das ist zu dunkel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
